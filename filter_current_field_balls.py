58601a0b9f9d632f72c8a179958ea2b0160df863
chengzhuo Long <2453064285@qq.com>
2024-07-17 18:00:47 +0800

    继续增加删球逻辑

--- filter_current_field_balls.py
+++ filter_current_field_balls.py
@@ -13,18 +13,18 @@ def time_to_start(end_time):
     :return: 下一回合的开始帧
     """
     global start_time, num_clip, exist_start
-    if end_time >= (len(df) - 19):
+    if end_time >= (len(df) - start_window):
         start_time = end_time
         exist_start = 0
         return start_time, exist_start
-    for i in range(end_time, len(df) - 19):
+    for i in range(end_time, len(df) - (start_window - 1)):
         # 如果到达末尾，直接返回
-        if i + 20 == len(df):
+        if i + start_window == len(df):
             start_time = end_time
             exist_start = 0
             return start_time, exist_start
         # 计算当前位置及其后20行的第二列总和
-        sum_col2 = df.iloc[i:i + 20, 1].sum()
+        sum_col2 = df.iloc[i:i + start_window, 1].sum()
         # 如果总和大于等于9
         if sum_col2 >= 9:
             # 回合序数迭代
@@ -50,25 +50,25 @@ def time_to_end(start_time):
     :return: 当前回合的结束帧
     """
     global end_time, exist_end
-    if start_time >= (len(df) - 59):
+    if start_time >= (len(df) - end_window):
         end_time = len(df)
         exist_end = 0
         return end_time, exist_end
-    for i in range(start_time, len(df) - 59):
+    for i in range(start_time, len(df) - (end_window - 1)):
         # 如果到达末尾，直接返回
-        if i + 60 == len(df):
+        if i + end_window == len(df):
             end_time = len(df)
             exist_end = 0
             return end_time, exist_end
-        # 计算当前位置及其后60行的第二列总和
-        sum_col2 = df.iloc[i:i + 60, 1].sum()
+        # 计算当前位置及其后100行的第二列总和
+        sum_col2 = df.iloc[i:i + end_window, 1].sum()
         # 如果总和小于10
         if sum_col2 < 10:
-            if i+30 >= len(df) - 1:
+            if i+40 >= len(df):
                 end_time = len(df)
             else:
                 # 输出当前行后30行的第一列的数值
-                end_time = df.iloc[i + 30, 0]
+                end_time = df.iloc[i + 40, 0]
             print(f"第 {num_clip} 个clip的结束帧是{end_time}")
             exist_end = 1
             # 如果切片时间达到要求，在终端进行裁剪
@@ -103,17 +103,17 @@ def insert_frame(insert_start_time):
     :return: 无
     """
     # 逐渐变小阶段
-    for i in range(insert_start_time, len(df) - 31):
+    for i in range(insert_start_time, len(df) - 16):
         # 下一个片段开始信号
         next_segment_begins = 0
         # 砍两刀
-        x_within_allowed_range = width * x_constraint_ratio < df.iloc[i, 2] < df.iloc[i + 1, 2] < width * (
-                    1 - x_constraint_ratio) or width * x_constraint_ratio < df.iloc[i + 1, 2] < df.iloc[
+        x_within_allowed_range = width * x_constraint_ratio < df.iloc[i, 2] <= df.iloc[i + 1, 2] < width * (
+                    1 - x_constraint_ratio) or width * x_constraint_ratio < df.iloc[i + 1, 2] <= df.iloc[
             i, 2] < width * (1 - x_constraint_ratio)
         # 如果出现y值小于70且连续一帧内变小，则准备插帧
         if 70 > df.iloc[i, 3] > df.iloc[i + 1, 3] > 0 and x_within_allowed_range:
             # 最多插30帧
-            for j in range(i + 2, i + 32):
+            for j in range(i + 2, i + 17):
                 # 在连续变小之后，从第三帧开始，若碰到y值和x值为0的帧，则将其visibility置1
                 if df.iloc[j, 2] == 0 and df.iloc[j, 3] == 0:
                     df.iloc[j, 1] = 1
@@ -122,70 +122,78 @@ def insert_frame(insert_start_time):
                 # 有球，且下一个片段已经开始了，则结束插帧
                 elif (df.iloc[j, 2] != 0 or df.iloc[j, 3] != 0) and next_segment_begins == 1:
                     break
+                elif j == i + 12 and next_segment_begins == 0:
+                    break
     # 逐渐增大阶段
     for i in range(insert_start_time, len(df) - 1):
         # 上一个片段开始信号
         pre_segment_begins = 0
         # 砍两刀
-        x_within_allowed_range = width * x_constraint_ratio < df.iloc[i, 2] < df.iloc[i + 1, 2] < width * (
-                1 - x_constraint_ratio) or width * x_constraint_ratio < df.iloc[i + 1, 2] < df.iloc[
+        x_within_allowed_range = width * x_constraint_ratio < df.iloc[i, 2] <= df.iloc[i + 1, 2] < width * (
+                1 - x_constraint_ratio) or width * x_constraint_ratio < df.iloc[i + 1, 2] <= df.iloc[
                                      i, 2] < width * (1 - x_constraint_ratio)
         # 30帧之后再往上插帧，最多插30帧
-        if i >= 30:
+        if i >= 15:
             if 70 > df.iloc[i + 1, 3] > df.iloc[i, 3] > 0 and x_within_allowed_range:
-                for j in reversed(range(i - 30, i + 1)):
+                for j in reversed(range(i - 15, i)):
                     if df.iloc[j, 2] == 0 and df.iloc[j, 3] == 0:
                         df.iloc[j, 1] = 1
                         pre_segment_begins = 1
                     elif (df.iloc[j, 2] != 0 or df.iloc[j, 3] != 0) and pre_segment_begins == 1:
                         break
+                    elif j == i - 10 and pre_segment_begins == 0:
+                        break
         # 30帧之内往上插帧，最多插到开头
         else:
             if 70 > df.iloc[i + 1, 3] > df.iloc[i, 3] > 0 and x_within_allowed_range:
-                for j in reversed(range(0, i + 1)):
+                for j in reversed(range(0, i)):
                     if df.iloc[j, 2] == 0 and df.iloc[j, 3] == 0:
                         df.iloc[j, 1] = 1
                         pre_segment_begins = 1
                     elif (df.iloc[j, 2] != 0 or df.iloc[j, 3] != 0) and pre_segment_begins == 1:
+                        break
+                    elif j == i - 10 and pre_segment_begins == 0:
                         break
 
 
 def delete_wrong_balls(delete_start_time):
     def check_repeats(group):
-        col3_counts = group.iloc[:, 2].value_counts()
-        col4_counts = group.iloc[:, 3].value_counts()
 
-        # 找出重复次数超过5次的元素
-        col3_to_zero = col3_counts[col3_counts > 5].index.tolist()
-        col4_to_zero = col4_counts[col4_counts > 5].index.tolist()
+        duplicated_rows_index = group.iloc[:, 1:4].duplicated(keep=False)
 
-        # 将满足条件的行的第2、3、4列设置为0
-        mask = (group.iloc[:, 2].isin(col3_to_zero)) | (group.iloc[:, 3].isin(col4_to_zero))
-        group.iloc[mask, 1] = 0
-        group.iloc[mask, 2] = 0
-        group.iloc[mask, 3] = 0
 
+        # 将这些行的值置为0
+        group.iloc[duplicated_rows_index, 5] = 1
         return group
 
-    # 使用rolling窗口，但是由于rolling窗口不适用于非数值类型的数据，
-    # 我们需要手动实现类似的功能
-    window_size = 20
+    def check_wrong_ball(group):
+        # 除了中间两个，其他位置都没有球，则清空整个窗口
+        balls_counts = group.iloc[:8, 1].sum() + group.iloc[13:, 1].sum()
+        if balls_counts == 0:
+            group.iloc[:, 5] = 1
+        return group
+
     for i in range(delete_start_time, len(df)):
-        if i == len(df) - 21:
-            # 取出一个窗口大小的数据块
+        if i == len(df) - delete_window:
             window_df = df.iloc[i:len(df)]
-            # 应用我们的检查函数
-            updated_window_df = check_repeats(window_df.copy())
-            # 将结果放回原DataFrame
-            df.iloc[i:len(df)] = updated_window_df
+            df.iloc[i:i + delete_window] = check_repeats(window_df.copy())
             break
         else:
-            # 取出一个窗口大小的数据块
-            window_df = df.iloc[i:i + window_size]
-            # 应用我们的检查函数
-            updated_window_df = check_repeats(window_df.copy())
-            # 将结果放回原DataFrame
-            df.iloc[i:i + window_size] = updated_window_df
+            window_df = df.iloc[i:i + delete_window]
+            df.iloc[i:i + delete_window] = check_repeats(window_df.copy())
+    repeats_mask = df.iloc[:, 5] == 1
+    df.iloc[repeats_mask, 1:4] = 0
+
+    for i in range(delete_start_time, len(df)):
+        if i == len(df) - delete_window:
+            window_df = df.iloc[i:len(df)]
+            df.iloc[i:len(df)] = check_wrong_ball(window_df.copy())
+            break
+        else:
+            window_df = df.iloc[i:i + delete_window]
+            df.iloc[i:i + delete_window] = check_wrong_ball(window_df.copy())
+    repeats_mask = df.iloc[:, 5] == 1
+    df.iloc[repeats_mask, 1:4] = 0
 
 
 def main(args):
@@ -215,16 +223,22 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="找出每一回合")
-    parser.add_argument("--input_csv_path", type=str, default='skcourt2_ball.csv', help="需处理表格的路径")
-    parser.add_argument("--output_csv_path", type=str, default='skcourt2_ballnew.csv', help="输出表格的路径")
-    parser.add_argument("--inputmp4_path", type=str, default='skcourt2_pred7.mp4', help="输入视频路径")
+    parser.add_argument("--input_csv_path", type=str, default='xyscourt2_ball.csv', help="需处理表格的路径")
+    parser.add_argument("--output_csv_path", type=str, default='xyscourt2_ballnew.csv', help="输出表格的路径")
+    parser.add_argument("--inputmp4_path", type=str, default='xyscourt2_pred3.mp4', help="输入视频路径")
     parser.add_argument("--slice_shortest_time", type=float, default=3.4, help="切片最短时间,单位秒")
     parser.add_argument("--x_constraint_ratio", type=float, default=0.29, help="插帧范围限制系数，避免其他场球干扰")
+    parser.add_argument("--start_window", type=int, default=20, help="滑动窗口检测开始帧")
+    parser.add_argument("--end_window", type=int, default=60, help="滑动窗口检测结束帧")
+    parser.add_argument("--delete_window", type=int, default=100, help="滑动窗口删除重复球")
+
     args = parser.parse_args()
     inputmp4_path = args.inputmp4_path
     slice_shortest_time = args.slice_shortest_time
     x_constraint_ratio = args.x_constraint_ratio
-
+    start_window = args.start_window
+    end_window = args.end_window
+    delete_window = args.delete_window
     # 获取视频宽度，方便“砍两刀”
     cap = cv2.VideoCapture(inputmp4_path)
     # 检查是否成功打开视频文件
@@ -241,4 +255,5 @@ if __name__ == "__main__":
     df = pd.read_csv(args.input_csv_path)
     # 在原csv中增加第五列
     df['开始和结束'] = None
+    df['标记需要清空的列表'] = None
     main(args)
